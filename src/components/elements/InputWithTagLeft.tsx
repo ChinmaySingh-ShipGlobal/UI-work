99b19fc44bab67b50b241e8d03fcfca22ec88bf6
ChinmaySingh-ShipGlobal <chinmay.singh@shipglobal.in>
2024-07-22 17:33:15 +0530

    Code for review after changes

--- src/components/elements/InputWithTagLeft.tsx
+++ src/components/elements/InputWithTagLeft.tsx
@@ -3,12 +3,12 @@ interface TextFieldAndValueProps {
   title: string;
   tag: string;
   placeholder?: string;
-  classNameDiv?: string;
+  divStyles?: string;
 }
 
-const InputWithTagLeft: React.FC<TextFieldAndValueProps> = ({ title, tag, placeholder, classNameDiv }) => {
+const InputWithTagLeft: React.FC<TextFieldAndValueProps> = ({ title, tag, placeholder, divStyles }) => {
   return (
-    <div className={classNameDiv}>
+    <div className={divStyles}>
       <p className="text-xs inline font-normal font-poppins">{title}</p>
       <div className="flex flex-row items-center mt-1">
         <p className="bg-gray-200 border-gray-150 border-r-0  rounded-l rounded-r-none p-1.5 font-normal text-sm border w-12 h-9 text-center">

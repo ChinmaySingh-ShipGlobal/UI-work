21da60a61eae14132cc10c6bca4afe2ed766c4b1
ChinmaySingh-ShipGlobal <chinmay.singh@shipglobal.in>
2024-07-26 10:56:42 +0530

    Added OrderDetails Module linked to Wallet History

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ProfileSettings from "./pages/ProfileSettings";
 import WalletHistory from "./pages/WalletHistory";
+import OrderDetails from "./pages/OrderDetails";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/profile", element: <ProfileSettings /> },
   { path: "/wallethistory", element: <WalletHistory /> },
+  { path: "/orderDetails", element: <OrderDetails /> },
 ]);
 function App() {
   return <RouterProvider router={router} />;
